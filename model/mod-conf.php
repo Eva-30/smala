26334b73ca7f03d5a2b0ce120bd4c6a27fc90875
Eva-30 <74191613+Eva-30@users.noreply.github.com>
2021-02-24 13:52:52 +0100

    Update mod-conf.php

--- model/mod-conf.php
+++ model/mod-conf.php
@@ -2,8 +2,8 @@
 
   const DB_HOST    = "localhost";
   const DB_NAME    = "smala";
-  const DB_LOGIN   = "evab";
-  const DB_PASS    = "paprika";
+  const DB_LOGIN   = "Martine";
+  const DB_PASS    = "mdp";
 
   const DB_DRIVER  = "mysql";
   const DB_CHARSET = "utf8mb4";
